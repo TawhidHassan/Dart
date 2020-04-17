7e4be42391adaec02c20766c309111c5036638d1
sifat hassan <sifat27.sh22@gmail.com>
2020-04-17 14:19:25 +0600

    make file run 1st programe

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dartPrograme/dartPrograme.dart' as dartPrograme;
+
+void main(List<String> arguments) {
+  print('Hello world: sifat');
+}
---
+++ test/dartPrograme_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartPrograme/dartPrograme.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
