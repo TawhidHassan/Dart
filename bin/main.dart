b23ad9c6e77b26f3dda6b5a478f064461ca92fea
sifat hassan <sifat27.sh22@gmail.com>
2020-04-19 14:26:48 +0600

    if else

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,13 @@
 import 'package:dartPrograme/dartPrograme.dart' as dartPrograme;
 
 void main(List<String> arguments) {
-  print('Hello world: sifat');
+ //if //else
+  var sales=330;
+  if(sales>100)
+    {
+      print("good sales");
+
+    }else{
+    print("bad sales");
+  }
 }
