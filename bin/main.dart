e412b7ab359f915c2d4c38b388677fc04070dfcc
sifat hassan <sifat27.sh22@gmail.com>
2020-04-19 14:36:21 +0600

    function define

--- bin/main.dart
+++ bin/main.dart
@@ -2,12 +2,10 @@ import 'package:dartPrograme/dartPrograme.dart' as dartPrograme;
 
 void main(List<String> arguments) {
  //if //else
-  var sales=330;
-  if(sales>100)
-    {
-      print("good sales");
-
-    }else{
-    print("bad sales");
-  }
+  var sales="sifat";
+  priny_x(sales);
+}
+priny_x(String ss)
+{
+  print(ss);
 }
