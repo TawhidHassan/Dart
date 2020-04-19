1aff0a8818959f66aa90a10ac7914f25a4bdcf15
sifat hassan <sifat27.sh22@gmail.com>
2020-04-19 15:08:55 +0600

    in drt ypu defain your function out of main function and use fat arrow'=>' to sourcat code

--- bin/main.dart
+++ bin/main.dart
@@ -3,9 +3,7 @@ import 'package:dartPrograme/dartPrograme.dart' as dartPrograme;
 void main(List<String> arguments) {
  //if //else
   var sales="sifat";
-  priny_x(sales);
+  print(priny_x(sales));
 }
-priny_x(String ss)
-{
-  print(ss);
-}
+priny_x(String ss)=>ss;
+
